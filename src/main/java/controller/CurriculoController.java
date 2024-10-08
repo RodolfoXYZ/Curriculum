caf30032791b01d5f15deb250344b7f391bb59a3
RodolfoXYZ <contato.rodolforodrigo@gmail.com>
2024-10-08 16:21:17 -0300

    fix controller

--- src/main/java/controller/CurriculoController.java
+++ src/main/java/controller/CurriculoController.java
@@ -3,6 +3,7 @@ package controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import model.CurriculumModel;
 import service.CurriculumService;
 
+
+@CrossOrigin(origins = "*")
 @RestController
 public class CurriculoController {
     @Autowired
@@ -45,7 +48,5 @@ public class CurriculoController {
         catch(Exception e){
             return false;
         }
-    }
-
-    
+    } 
 }
